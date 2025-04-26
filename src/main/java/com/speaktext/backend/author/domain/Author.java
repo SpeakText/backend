2f164ec88f5bc1311a47886e2427749908955fd7
한석진 <seokjin.han010@gmail.com>
2025-04-26 17:58:06 +0900

    feat : Author 엔티티 추가

---
+++ src/main/java/com/speaktext/backend/author/domain/Author.java
@@ -0,0 +1,24 @@
+package com.speaktext.backend.author.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long authorId;
+
+    @Column(nullable = false, unique = true, length = 64)
+    private String id;
+
+    @Column(nullable = false, length = 64)
+    private String password;
+
+    @Column(nullable = false, length = 64)
+    private String name;
+
+    @Column(nullable = false, length = 200)
+    private String email;
+
+}
